339fcd2d51fede732485c20549e8abc51c8a74d4
Hüseyin Kazım Tosun <huseyinkazim@hotmail.com>
2019-04-10 11:47:30 +0300

    2.deploy

--- TCKimlikSorgula/Program.cs
+++ TCKimlikSorgula/Program.cs
@@ -11,6 +11,10 @@ namespace TCKimlikSorgula
     class Program
     {
         private static KPSService.KPSPublicSoapClient KPSServiceClient = null;
+        static long TCKmlikNo;
+        static string name { set; get; }
+        static string lastname { set; get; }
+        static int birthDayYear ; 
         private static void CreateServiceClient()
         {
             BasicHttpBinding binding = new BasicHttpBinding();
@@ -22,27 +26,64 @@ namespace TCKimlikSorgula
 
             KPSServiceClient = new KPSService.KPSPublicSoapClient(binding, address);
         }
+        private static void DegerAl()
+        {
+            bool uygun;
+            do
+            {
+                Console.WriteLine("Lütfen Tc Kimlik numaranızı giriniz:");
+                var tc = Console.ReadLine();
+                uygun = long.TryParse(tc, out TCKmlikNo);
+
+                uygun = uygun ? tc.Length == 11 : uygun;
+                if (!uygun)
+                    Console.WriteLine("Gerçek bir Tc Kimlik giriniz.");
+            } while (!uygun);
+            Console.WriteLine("Lütfen Adınızı giriniz:");
+            name = Console.ReadLine();
+            Console.WriteLine("Lütfen Soyadınızı giriniz:");
+            lastname = Console.ReadLine();
+
+            uygun = false;
+            do
+            {
+                Console.WriteLine("Lütfen Doğum yılınızı giriniz:");
+                var birthyear = Console.ReadLine();
+                uygun=int.TryParse(birthyear, out birthDayYear);
+                uygun = uygun ?birthyear.Length==4:uygun;
+                if(!uygun)
+                    Console.WriteLine("Lütfen doğum yılınızı kontrol edin.");
+
+
+            } while (!uygun);
+
+        }
         static void Main(string[] args)
         {
-            try {
-                var x = System.Net.ServicePointManager.SecurityProtocol;//DEFAULT TLS=192
-                System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+            try
+            {
+                do
+                {
+                    var x = System.Net.ServicePointManager.SecurityProtocol;//DEFAULT TLS=192
+                    System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
 
-            CreateServiceClient();
-            if (KPSServiceClient == null)
-                throw new Exception("Cannot Create An Instance Of KPS Service");
+                    CreateServiceClient();
+                    if (KPSServiceClient == null)
+                        throw new Exception("Cannot Create An Instance Of KPS Service");
 
-            CultureInfo culture = new CultureInfo("tr-TR", false);
+                    CultureInfo culture = new CultureInfo("tr-TR", false);
 
-                //You must imlement this place
-            long TCKmlikNo;
-            string name;
-            string lastname;
-            int birthDayYear;
+                    //You must imlement this place
 
+                    DegerAl();
 
+                    var response = KPSServiceClient.TCKimlikNoDogrula(TCKmlikNo, name.ToUpper(culture), lastname.ToUpper(culture), birthDayYear);
+                    if (response)
+                        Console.WriteLine("Türkiye Cumhuriyetinde girdiğiniz değerlerle bir kayıt bulunmaktadır");
+                    else
+                        Console.WriteLine("Türkiye Cumhuriyetinde girdiğiniz değerlerle bir kayıt bulunmamaktadır");
 
-            var response = KPSServiceClient.TCKimlikNoDogrula(TCKmlikNo, name.ToUpper(culture), lastname.ToUpper(culture), birthDayYear);
+                } while (true);
             }
             catch (Exception ex)
             {
